c4dbfd165c5fd3518410f91795d2426783e9dc03
HubTakenIs <hubertaslol@gmail.com>
2025-07-30 22:24:51 +0100

    rate limit

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(CircleShape):
         super().__init__(x, y, PLAYER_RADIUS)
 
         self.rotation = 0
+        self.timer = 0
 
     # in the player class
     def triangle(self):
@@ -41,7 +42,10 @@ class Player(CircleShape):
             self.move(-dt)
         
         if keys[pygame.K_SPACE]:
-            self.shoot()
+            if self.timer < 0:
+                self.shoot()
+                self.timer = PLAYER_SHOOT_COOLDOWN
+        self.timer -= dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
